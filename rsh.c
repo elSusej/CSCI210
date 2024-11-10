2092d15ffcfb1233b8ea5f4e118207254393f0b4
Jesus <jesus.ledezmatuba@gmail.com>
2024-11-10 16:19:45 -0700

    S

--- rsh.c
+++ rsh.c
@@ -1,8 +1,3 @@
-// Resources used: Stackoverflow (Linked), 
-// week 10 lecture posix, waitpid, perror, week 8 for strings. 
-// https://cplusplus.com/reference/ for general (I know its C++ but it works for C i found out)
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <spawn.h>
@@ -16,22 +11,17 @@ extern char **environ;
 
 char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
 
-int isAllowed(const char*cmd) {
-	// TODO
-	// return 1 if cmd is one of the allowed commands
-	// return 0 otherwise
-	
-	// https://stackoverflow.com/questions/8004237/how-do-i-properly-compare-strings-in-c
-	// https://stackoverflow.com/questions/1597830/iterate-through-a-c-array
-
-    for (size_t i = 0; i < sizeof(allowed) / sizeof(allowed[0]); i++) {
-        // Check if cmd matches any allowed command
-        if (strcmp(allowed[i], cmd) == 0) {
-            return 1;  // Command is allowed
+int isAllowed(const char* cmd) {
+    // TODO
+    // return 1 if cmd is one of the allowed commands
+    // return 0 otherwise
+    for (int i = 0; i < 12; i++) {
+        if ( strcmp(cmd,allowed[i]) == 0 ) {
+            return 1;
         }
     }
-    return 0;  // Command is not allowed
-
+    
+    return 0;
 }
 
 int main() {
@@ -40,93 +30,72 @@ int main() {
     // Add variables as needed
 
     char line[256];
-	char *args[21];  // max 20 arguments + NULL terminator
-    char line_copy[256];
+    char linecpy[256];
+    char* argv[20];
+    char* token;
+	pid_t pid;
     int status;
-    pid_t pid;
-
+    posix_spawnattr_t attr;
+	posix_spawnattr_init(&attr);
 
     while (1) {
 
-	// No indents?????? Yall want to see the world burn.
-	fprintf(stderr,"rsh>");
+    fprintf(stdout,"rsh>");
 
-	if (fgets(line,256,stdin)==NULL) continue;
+    if (fgets(line,256,stdin)==NULL) continue;
 
-	if (strcmp(line,"\n")==0) continue;
+    if (strcmp(line,"\n")==0) continue;
 
-	line[strlen(line)-1]='\0';
+    line[strlen(line)-1]='\0';
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+    strcpy(linecpy, line);
+    token = strtok(linecpy, " ");
+    int i = 0;
 
-		strcpy(line_copy, line);  // Make a copy for second tokenization if needed
-        
-        // Tokenize input
-        char *token = strtok(line, " ");
-        int arg_count = 0;
-        
-        // Get command and arguments
-        while (token != NULL && arg_count < 20) {
-            args[arg_count++] = token;
-            token = strtok(NULL, " ");
+    while (token != NULL) {
+        argv[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+
+    argv[i] = NULL;
+
+    if (isAllowed(argv[0]) == 0) {
+        printf("NOT ALLOWED!\n");
+        continue;
+    }
+    else if (strcmp(argv[0],"exit") == 0) {
+        break;
+    }
+    else if (strcmp(argv[0],"help") == 0) {
+        printf("The allowed commands are:\n");
+        for (int i = 1; i <= 12; i++) {
+            printf("%d: %s\n", i, allowed[i-1]);
         }
-		
-        args[arg_count] = NULL;  // NULL terminate the argument array
-        
-        // Check if command is allowed
-        if (!isAllowed(args[0])) {
-            printf("NOT ALLOWED!\n");
+        continue;
+    }
+    else if (strcmp(argv[0],"cd") == 0) { //argv[1] != NULL) && (argv[2] != NULL)
+        if (i > 2) { //more than 1 argument
+            printf("-rsh: cd: too many arguments");
+        }
+        else if (i == 1){ //no argument
+            chdir(getenv("HOME"));
+        }
+        else{ //exactly 1 argument
+            chdir(argv[1]);
+        }
+        continue;
+    }
+
+    status = posix_spawnp(&pid, argv[0], NULL, &attr, argv, environ);
+
+    if (status == 0) {
+        if (waitpid(pid, &status, 0) == -1) {
             continue;
-        }
-        
-        // Handle built-in commands
-        if (strcmp(args[0], "exit") == 0) {
-            return 0;
-        }
-        else if (strcmp(args[0], "help") == 0) {
-            printf("The allowed commands are:\n");
-            printf("1: cp\n");
-            printf("2: touch\n");
-            printf("3: mkdir\n");
-            printf("4: ls\n");
-            printf("5: pwd\n");
-            printf("6: cat\n");
-            printf("7: grep\n");
-            printf("8: chmod\n");
-            printf("9: diff\n");
-            printf("10: cd\n");
-            printf("11: exit\n");
-            printf("12: help\n");
-            continue;
-        }
-        else if (strcmp(args[0], "cd") == 0) {
-            if (arg_count > 2) {  // More than one argument for cd
-                printf("-rsh: cd: too many arguments\n"); 
-            }
-            else if (arg_count == 1) {  // No argument, go to home directory
-                chdir(getenv("HOME"));
-            }
-            else {  // One argument
-                if (chdir(args[1]) != 0) {
-                    perror("cd");
-                }
-            }
-            continue;
-        }
-        
-        // Spawn process for allowed external commands
-        status = posix_spawnp(&pid, args[0], NULL, NULL, args, environ); // (Similar to `posix_spawn' but search for FILE in the PATH.)
-        
-        if (status == 0) {
-            if (waitpid(pid, &status, 0) != -1) { // Wait for spawned process to finish. (Ed-discussion common errors) Returns -1 if unsucc.
-                continue;
-            }
         }
     }
 
+    posix_spawnattr_destroy(&attr);
+    }
     return 0;
-
 }
