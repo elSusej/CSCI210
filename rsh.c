b5e98e3a40a5a14529c441058455bfc1a481590f
Jesus <jesus.ledezmatuba@gmail.com>
2024-11-10 15:49:06 -0700

    s

--- rsh.c
+++ rsh.c
@@ -1,3 +1,8 @@
+// Resources used: Stackoverflow (Linked), 
+// week 10 lecture posix, waitpid, perror, week 8 for strings. 
+// https://cplusplus.com/reference/ for general (I know its C++ but it works for C i found out)
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <spawn.h>
@@ -11,17 +16,22 @@ extern char **environ;
 
 char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
 
-int isAllowed(const char* cmd) {
-    // TODO
-    // return 1 if cmd is one of the allowed commands
-    // return 0 otherwise
-    for (int i = 0; i < 12; i++) {
-        if ( strcmp(cmd,allowed[i]) == 0 ) {
-            return 1;
+int isAllowed(const char*cmd) {
+	// TODO
+	// return 1 if cmd is one of the allowed commands
+	// return 0 otherwise
+	
+	// https://stackoverflow.com/questions/8004237/how-do-i-properly-compare-strings-in-c
+	// https://stackoverflow.com/questions/1597830/iterate-through-a-c-array
+
+    for (size_t i = 0; i < sizeof(allowed) / sizeof(allowed[0]); i++) {
+        // Check if cmd matches any allowed command
+        if (strcmp(allowed[i], cmd) == 0) {
+            return 1;  // Command is allowed
         }
     }
-    
-    return 0;
+    return 0;  // Command is not allowed
+
 }
 
 int main() {
@@ -30,191 +40,93 @@ int main() {
     // Add variables as needed
 
     char line[256];
-    char linecpy[256];
-    char* argv[20];
-    char* token;
-	pid_t pid;
+	char *args[21];  // max 20 arguments + NULL terminator
+    char line_copy[256];
     int status;
-    posix_spawnattr_t attr;
-	posix_spawnattr_init(&attr);
+    pid_t pid;
+
 
     while (1) {
 
-    fprintf(stdout,"rsh>");
+	// No indents?????? Yall want to see the world burn.
+	fprintf(stderr,"rsh>");
 
-    if (fgets(line,256,stdin)==NULL) continue;
+	if (fgets(line,256,stdin)==NULL) continue;
 
-    if (strcmp(line,"\n")==0) continue;
+	if (strcmp(line,"\n")==0) continue;
 
-    line[strlen(line)-1]='\0';
+	line[strlen(line)-1]='\0';
 
-    strcpy(linecpy, line);
-    token = strtok(linecpy, " ");
-    int i = 0;
+	// TODO
+	// Add code to spawn processes for the first 9 commands
+	// And add code to execute cd, exit, help commands
+	// Use the example provided in myspawn.c
 
-    while (token != NULL) {
-        argv[i] = token;
-        token = strtok(NULL, " ");
-        i++;
-    }
-
-    argv[i] = NULL;
-
-    if (isAllowed(argv[0]) == 0) {
-        printf("NOT ALLOWED!\n");
-    }
-    // TODO
-    // Add code to spawn processes for the first 9 commands
-    // And add code to execute cd, exit, help command
-    
-    if (strcmp(argv[0],"cp") == 0) {
-        if (posix_spawnp(&pid, "cp", NULL, &attr, argv, environ) != 0) {
-            perror("spawn failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Wait for the spawned process to terminate
-        if (waitpid(pid, &status, 0) == -1) {
-            perror("waitpid failed");
-            exit(EXIT_FAILURE);
-        }
-
-        if (WIFEXITED(status)) {
-            printf("Spawned process exited with status %d\n", WEXITSTATUS(status));
-        }
-
-        // Destroy spawn attributes
-        posix_spawnattr_destroy(&attr);
-    }
-    if (strcmp(argv[0],"touch") == 0) {
-        if (posix_spawnp(&pid, "touch", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
+		strcpy(line_copy, line);  // Make a copy for second tokenization if needed
         
-    }
-    if (strcmp(argv[0],"mkdir") == 0) {
-        if (posix_spawnp(&pid, "mkdir", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
-    }
-
-    if (strcmp(argv[0],"ls") == 0) {
-        if (posix_spawnp(&pid, "ls", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
-    }
-
-    if (strcmp(argv[0],"pwd") == 0) {
-        if (posix_spawnp(&pid, "pwd", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
-    }
-    if (strcmp(argv[0],"cat") == 0) {
-        if (posix_spawnp(&pid, "cat", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
-    }
-    if (strcmp(argv[0],"grep") == 0) {
-        if (posix_spawnp(&pid, "grep", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
+        // Tokenize input
+        char *token = strtok(line, " ");
+        int arg_count = 0;
         
-    }
-    if (strcmp(argv[0],"chmod") == 0) {
-        if (posix_spawnp(&pid, "chmod", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
+        // Get command and arguments
+        while (token != NULL && arg_count < 20) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+		
+        args[arg_count] = NULL;  // NULL terminate the argument array
         
-    }
-    if (strcmp(argv[0],"diff") == 0) {
-        if (posix_spawnp(&pid, "diff", NULL, &attr, argv, environ) != 0) {
-        perror("spawn failed");
-        exit(EXIT_FAILURE);
+        // Check if command is allowed
+        if (!isAllowed(args[0])) {
+            printf("NOT ALLOWED!\n");
+            continue;
+        }
+        
+        // Handle built-in commands
+        if (strcmp(args[0], "exit") == 0) {
+            return 0;
+        }
+        else if (strcmp(args[0], "help") == 0) {
+            printf("The allowed commands are:\n");
+            printf("1: cp\n");
+            printf("2: touch\n");
+            printf("3: mkdir\n");
+            printf("4: ls\n");
+            printf("5: pwd\n");
+            printf("6: cat\n");
+            printf("7: grep\n");
+            printf("8: chmod\n");
+            printf("9: diff\n");
+            printf("10: cd\n");
+            printf("11: exit\n");
+            printf("12: help\n");
+            continue;
+        }
+        else if (strcmp(args[0], "cd") == 0) {
+            if (arg_count > 2) {  // More than one argument for cd
+                printf("-rsh: cd: too many arguments\n"); 
+            }
+            else if (arg_count == 1) {  // No argument, go to home directory
+                chdir(getenv("HOME"));
+            }
+            else {  // One argument
+                if (chdir(args[1]) != 0) {
+                    perror("cd");
+                }
+            }
+            continue;
+        }
+        
+        // Spawn process for allowed external commands
+        status = posix_spawnp(&pid, args[0], NULL, NULL, args, environ); // (Similar to `posix_spawn' but search for FILE in the PATH.)
+        
+        if (status == 0) {
+            if (waitpid(pid, &status, 0) != -1) { // Wait for spawned process to finish. (Ed-discussion common errors) Returns -1 if unsucc.
+                continue;
+            }
+        }
     }
 
-    if (waitpid(pid, &status, 0) == -1) {
-        perror("waitpid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    posix_spawnattr_destroy(&attr);
-    }
-    
-    if (strcmp(argv[0],"cd") == 0) {
-        if ((argv[1] != NULL) && (argv[2] != NULL)) {
-            printf("-rsh: cd: too many arguments");
-        }
-        else {
-            chdir(argv[1]);
-        }
-    }
-    if (strcmp(argv[0],"exit") == 0) {
-        break;
-    }
-    if (strcmp(argv[0],"help") == 0) {
-        printf("The allowed commands are:\n");
-        for (int i = 1; i <= 12; i++) {
-            printf("%d: %s\n", i, allowed[i-1]);
-        }
-    }
-    // Use the example provided in myspawn.c
-
-    }
     return 0;
+
 }
